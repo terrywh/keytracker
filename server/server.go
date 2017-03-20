dbd390a4b7711ef9a3f8b83b7e02b03f9e729321
terrywh <terry.wuhao@gmail.com>
2017-03-20 12:38:21 +0800

    修正：识别协议过程不应阻塞其他连接；

--- server/server.go
+++ server/server.go
@@ -31,18 +31,8 @@ func ListenAndServe(addr string, sh SessionHandler, hh http.Handler) {
 		}
 		conn := ccc.(*net.TCPConn)
 		initConn(conn)
-		// 至少发送一个字节数据后开始服务（用于识别协议）
-		rd := bufio.NewReader(ccc)
-		pbyte, err := rd.Peek(1)
-		if err != nil {
-			log.Println("[warning] socket diconnected before protocol detection")
-		} else if pbyte[0] == byte('{') {
-			// detect 协议
-			s := NewSession(&sessionConnPeeker{rd, conn}, conn.RemoteAddr().String())
-			go s.Start(sh)
-		}else{
-			httpListener.Push(rd, ccc)
-		}
+		go startConn(conn, sh, httpListener)
+		
 	}
 }
 
@@ -50,4 +40,19 @@ func initConn(conn *net.TCPConn) {
 	conn.SetKeepAlive(true)
 	conn.SetKeepAlivePeriod(15 * time.Second)
 	// conn.SetLinger(-1)
+}
+
+func startConn(conn *net.TCPConn, sh SessionHandler, hl httpListener) {
+	// 至少发送一个字节数据后开始服务（用于识别协议）
+	rd := bufio.NewReader(conn)
+	pbyte, err := rd.Peek(1)
+	if err != nil {
+		log.Println("[warning] socket diconnected before protocol detection")
+	} else if pbyte[0] == byte('{') {
+		// detect 协议
+		s := NewSession(&sessionConnPeeker{rd, conn}, conn.RemoteAddr().String())
+		go s.Start(sh)
+	}else{
+		hl.Push(rd, conn)
+	}
 }
